7b3a8751ea64d71f377ce8c8d060924ecd162da0
MrFacundo <ftroitero@proton.me>
2026-01-21 23:51:52 +0000

    updated alias

--- home/.oh-my-zsh/custom/aliases.zsh
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -17,8 +17,9 @@ alias ghr='(LC_ALL=en_US.UTF-8 firefox "https://github.com/MrFacundo?tab=reposit
 # other
 alias pngtojpg='magick mogrify  -format jpg -background white -alpha remove -alpha off *.png'
 alias c='code'
-alias l='eza -1 --icons=always'
-alias ls='eza -la --no-user'
+alias l='eza -1 --icons=always --group-directories-first'
+alias ls='eza -la --no-user --group-directories-first'
+alias lt='eza -F -T --level=3 --color=always --icons=always --group-directories-first'
 alias cd="z"
 alias ccss='sudo chown -R $(whoami) "$(which code)" && sudo chown -R $(whoami) /opt/visual-studio-code'
 alias krestart='killall plasmashell 2>/dev/null; kstart plasmashell'
@@ -27,9 +28,9 @@ alias pacman="sudo pacman"
 alias apt='sudo apt'
 alias v="nvim"
 if command -v bat &>/dev/null; then
-  alias cat='bat'
+  alias cat='bat --wrap auto -n -P'
 elif command -v batcat &>/dev/null; then
-  alias cat='batcat'
+  alias cat='batcat --wrap auto -n -P'
 fi
 alias dot='cd ~/dotfiles/ && gs'
 alias dev='npm run dev'

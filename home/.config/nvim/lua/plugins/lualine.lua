80afcac9e0ac8c2c409e7b8a3e8d46a3438ea872
Facundo Troitero <facundo.troitero@criticalsoftware.com>
2026-04-16 11:27:11 +0100

    yazi theme

---
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+# Remove clutter
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.sections.lualine_z = {}
+
+      opts.sections.lualine_x = {
+        { "diff" },
+      }
+
+      return opts
+    end,
+  },
+}
